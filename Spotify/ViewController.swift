b7aa85c1ca90a116de3a19b78a3e92fa36d705c8
Natalia Shchipakina <shchipakinanata1i@gmail.com>
2023-07-03 13:22:16 +0300

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Natalia Shchipakina on 03.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
