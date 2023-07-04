00691f685ba3f9f25bdef9dd8aff41a3192b1fe3
NataliaShchipakina <shchipakinanata1i@gmail.com>
2023-07-04 20:43:52 +0300

    Spotify: Settings of Authorization and log in app

--- Spotify/Services/AuthManager.swift
+++ Spotify/Services/AuthManager.swift
@@ -11,10 +11,25 @@ final class AuthManager {
     
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "3c97f978fe294c9a8f333a584e9237c7"
+        static let clientSecret = "524152b77f8142f9a3a7c0ed4d7325eb"
+    }
+    
+    
     private init() {}
     
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "spotify-ios-quick-start://spotify-login-callback"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
+    
     var isSignedIn: Bool {
-        return true
+        return false
     }
     
     private var accessToken: String? {
@@ -31,5 +46,19 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchandeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get Token
+    }
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func casheToken() {
+        
     }
 }
